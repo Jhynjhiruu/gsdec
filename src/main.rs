d757106869489ef987e187d5831b6b6b6bbef79a
Jhynjhiruu <cub3r12@gmail.com>
2023-07-05 23:25:01 +0100

    Fix imports a bit

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use std::fs::{read, write};
 
 use args::{OperationMode, Options};
 
+use anyhow::Result;
+
 const KEY: [u32; 0x10] = [
     0x2E337114, 0x2E434981, 0x217B6975, 0x83785915, 0x35D42A1C, 0x34E8AD13, 0xB118DEE2, 0x3578BC51,
     0xD4328715, 0x1276D768, 0x41444255, 0x22FEF3D1, 0x9478EDAE, 0x12536834, 0x636526A3, 0x2EC12C45,
 ];
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
     let options = Options::parse();
 
     let filedata = read(options.infile)?;
